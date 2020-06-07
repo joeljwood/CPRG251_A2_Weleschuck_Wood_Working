cbc3253d42fa03a69102f59a29ebe1587292551f
ZennonWeleschuk <59177705+ZennonWeleschuk@users.noreply.github.com>
2020-06-07 02:15:29 -0600

    Update BookChildrens.java
    done

--- CPRG251_A2_Weleschuck_Wood/src/sait/bms/problemdomain/BookChildrens.java
+++ CPRG251_A2_Weleschuck_Wood/src/sait/bms/problemdomain/BookChildrens.java
@@ -1,75 +1,90 @@
 package sait.bms.problemdomain;
 
-
+/**
+ * This subclass extends the books superclass and is for children's book objects
+ * 
+ * @author Zennon and Joel
+ *
+ */
 public class BookChildrens extends Book {
 	private String author;
 	private char format;
-	
+
 	/**
+	 * constructor for children books
 	 * 
-	 * @param author
-	 * @param format
+	 * @param isbn       unique number for each book
+	 * @param callNumber used to sort physical books
+	 * @param available  number of book to take out
+	 * @param total      number of book library owns
+	 * @param title      of book
+	 * @param author     of childrens book
+	 * @param format     of childrens book
 	 */
-	public BookChildrens(String author, char format) {
-		super();
-		this.author = author;
-		this.format = format;
-	}
-
-	public BookChildrens() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public BookChildrens(long isbn, String callNumber, int available, int total, String title, String author, char format) {
+	public BookChildrens(long isbn, String callNumber, int available, int total, String title, String author,
+			char format) {
 		super(isbn, callNumber, available, total, title);
 		this.author = author;
 		this.format = format;
 	}
-	
-	
-	
+
+	/**
+	 * 
+	 * @return tha author of childrens book
+	 */
 	public String getAuthor() {
 		return author;
 	}
 
+	/**
+	 * 
+	 * @param author set of childrens book
+	 */
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
+	/**
+	 * 
+	 * @return short format of childrens book
+	 */
 	public char getFormat() {
 		return format;
 	}
 
+	/**
+	 * 
+	 * @param format set of childrens book
+	 */
 	public void setFormat(char format) {
 		this.format = format;
 	}
 
 	/**
-	 * takes the single char letter and returns the long name of the format
-	 * @param format code for children's book 
-	 * @return the long spelled out name
+	 * Takes the single char letter and returns the long name of the format
+	 * 
+	 * @return the long spelled out format of children's book
 	 */
 	public String formatLong() {
-		String tempFormatLong = "empty"; 
-		if (format == 'P'){
+		String tempFormatLong = "empty";
+		if (format == 'P') {
 			tempFormatLong = "Picture Book";
-		}else if (format == 'E'){
+		} else if (format == 'E') {
 			tempFormatLong = "Early Readers";
-		}else if (format == 'C') {
+		} else if (format == 'C') {
 			tempFormatLong = "Chapter Book";
 		}
 		return tempFormatLong;
 	}
-	
 
-	@Override 
+	/**
+	 * print formatted children's book object
+	 */
+	@Override
 	public String toString() {
-		return String.format("%-15s%d\n%-15s%s\n%-15s%d\n%-15s%d\n%-15s%s\n%-15s%s\n%-15s%s\n",
-				"ISBN:", getIsbn(), "Call Number:", getCallNumber(), "Available:", getAvailable(), "Total:", getTotal(), "Title:", getTitle(), "Author:", getAuthor(), "Format:", formatLong() );
+		return String.format("%-15s%d\n%-15s%s\n%-15s%d\n%-15s%d\n%-15s%s\n%-15s%s\n%-15s%s\n", "ISBN:", getIsbn(),
+				"Call Number:", getCallNumber(), "Available:", getAvailable(), "Total:", getTotal(), "Title:",
+				getTitle(), "Author:", getAuthor(), "Format:", formatLong());
 	}
-	
 
-	
-	
 }
