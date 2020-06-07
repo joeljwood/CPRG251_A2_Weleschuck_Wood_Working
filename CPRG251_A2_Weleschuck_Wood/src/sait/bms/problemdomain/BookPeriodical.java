acb9cf651695128191054041c2e8d0658d9d5b45
ZennonWeleschuk <59177705+ZennonWeleschuk@users.noreply.github.com>
2020-06-07 02:21:23 -0600

    Update BookPeriodical.java

--- CPRG251_A2_Weleschuck_Wood/src/sait/bms/problemdomain/BookPeriodical.java
+++ CPRG251_A2_Weleschuck_Wood/src/sait/bms/problemdomain/BookPeriodical.java
@@ -1,22 +1,52 @@
 package sait.bms.problemdomain;
 
-public class BookPeriodical extends Book{
+/**
+ * subclass for periodical, extends book superclass
+ * 
+ * @author Zennon and Joel
+ *
+ */
+public class BookPeriodical extends Book {
 
 	private char frequency;
-	
-	public BookPeriodical (long isbn, String callNumber, int available, int total, String title, char frequency) {
+
+	/**
+	 * constructor for periodical subclass
+	 * 
+	 * @param isbn       unique number for each book
+	 * @param callNumber used to sort physical books
+	 * @param available  number of book to take out
+	 * @param total      number of book library owns
+	 * @param title      of book
+	 * @param frequency  of publishing for periodicals
+	 */
+	public BookPeriodical(long isbn, String callNumber, int available, int total, String title, char frequency) {
 		super(isbn, callNumber, available, total, title);
 		this.frequency = frequency;
 	}
-	
+
+	/**
+	 * 
+	 * @return the frequency of periodicals
+	 */
+
 	public char getFrequency() {
 		return frequency;
 	}
 
-	public void setFrequency(char diet) {
+	/**
+	 * 
+	 * @param diet set the short format frequency for periodicals
+	 */
+	public void setFrequency(char frequency) {
 		this.frequency = frequency;
 	}
-	
+
+	/**
+	 * converts the short format of frequency to the long spelled out version
+	 * 
+	 * @return the long version of frequency
+	 */
 	public String frequencyLong() {
 		String tempFrequencyLong = "None";
 		if (frequency == 'D') {
@@ -27,16 +57,19 @@ public class BookPeriodical extends Book{
 			tempFrequencyLong = "Monthly";
 		} else if (frequency == 'B') {
 			tempFrequencyLong = "Bimonthly";
-		}
-		else if (frequency == 'Q') {
+		} else if (frequency == 'Q') {
 			tempFrequencyLong = "Quarterly";
 		}
 		return tempFrequencyLong;
 	}
+
+	/**
+	 * prints formatted output for periodical objects
+	 */
 	@Override
 	public String toString() {
 		return String.format("%-15s%d\n%-15s%s\n%-15s%d\n%-15s%d\n%-15s%s\n%-15s%s\n", "ISBN:", getIsbn(),
 				"Call Number:", getCallNumber(), "Available:", getAvailable(), "Total:", getTotal(), "Title:",
 				getTitle(), "Frequency", frequencyLong());
-}
+	}
 }
